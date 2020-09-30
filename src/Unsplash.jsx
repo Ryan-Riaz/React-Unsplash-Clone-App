9b6dc9dcb8c86c79c8fc43350920581eb530a9e9
Ryan-Riaz <devryanriaz@gmail.com>
2020-09-30 15:24:36 +0600

    initial unsplash clone project set up

---
+++ src/Unsplash.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Unsplash = () => {
+	const API_KEY = "199uMSNmn2t4j66K9_0feK8S8U0iuD_e6URjuA_fsn4";
+	return (
+		<div>
+			<h1>From Unsplash Component</h1>
+		</div>
+	);
+};
+
+export default Unsplash;
