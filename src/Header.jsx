8f868735fd77ecf65fcbbd42b2c0827358c6cb1e
Ryan-Riaz <devryanriaz@gmail.com>
2020-09-30 17:29:27 +0600

    full app uploaded into GitHub

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div className="header">
+			<h1>My Unsplash Clone Project</h1>
+		</div>
+	);
+};
+
+export default Header;
--- src/Unsplash.jsx
+++ src/Unsplash.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./Header";
 
 const Unsplash = () => {
-	const API_KEY = "199uMSNmn2t4j66K9_0feK8S8U0iuD_e6URjuA_fsn4";
+	const [keyword, setKeyWord] = useState("");
+	const [imgs, setImgs] = useState([]);
+	const viewImgs = () => {
+		const API_KEY = "199uMSNmn2t4j66K9_0feK8S8U0iuD_e6URjuA_fsn4";
+		const URL = `https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${keyword}&orientation=squarish`;
+		fetch(URL)
+			.then((res) => res.json())
+			.then((data) => {
+				setImgs(data.results);
+				setKeyWord("");
+				console.log(data);
+			});
+	};
+
 	return (
 		<div>
-			<h1>From Unsplash Component</h1>
+			<Header />
+			<input
+				value={keyword}
+				onChange={(e) => setKeyWord(e.target.value)}
+				className="searchBar"
+				type="text"
+				placeholder="Type any wallpaper name..."
+			/>
+			<button onClick={viewImgs} className="searchBtn">
+				SEARCH HERE
+			</button>
+			<div className="galleries">
+				{imgs.map((img) => (
+					<img
+						key={img.id}
+						src={img.urls.regular}
+						alt={img.alt_description}
+						title={img.alt_description}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
